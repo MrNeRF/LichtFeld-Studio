77bd55c77ef46aad43c9d571c7615f049a3bac70
Janusch Patas <janusch.patas@gmx.de>
2026-02-03 21:10:33 +0100

    fix

--- src/python/stubs/lichtfeld/build_info.pyi
+++ src/python/stubs/lichtfeld/build_info.pyi
@@ -1,7 +1,15 @@
-version: str
-commit: str
-build_type: str
-platform: str
-cuda_gl_interop: bool
-repo_url: str
-website_url: str
+
+
+version: str = 'v0.4.2-4-gf35c91aa'
+
+commit: str = 'f35c91aa'
+
+build_type: str = 'Release'
+
+platform: str = 'Linux'
+
+cuda_gl_interop: bool = True
+
+repo_url: str = 'https://github.com/MrNeRF/LichtFeld-Studio'
+
+website_url: str = 'https://lichtfeld.io'
