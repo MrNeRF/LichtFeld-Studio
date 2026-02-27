cded0d4972041b56a393bfbb5eec9f40cef65435
Janusch Patas <janusch.patas@gmx.de>
2026-02-27 11:01:51 +0100

    ci fixes

--- src/visualizer/gui/panels/menu_bar.cpp
+++ src/visualizer/gui/panels/menu_bar.cpp
@@ -178,7 +178,7 @@ namespace lfs::vis::gui {
         if (fonts_.regular)
             ImGui::PushFont(fonts_.regular);
 
-        static constexpr ImVec4 kTransparent{0.0f, 0.0f, 0.0f, 0.0f};
+        static const ImVec4 kTransparent(0.0f, 0.0f, 0.0f, 0.0f);
 
         // Bar background and text: transparent (RmlUi provides visuals)
         ImGui::PushStyleColor(ImGuiCol_MenuBarBg, kTransparent);
