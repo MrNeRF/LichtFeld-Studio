2869f6d27940da3804afe1a602b9e570dfa3ac35
Janusch Patas <janusch.patas@gmx.de>
2026-08-10 10:05:43 +0200

    disable nvImageCodec device allocator by default

--- src/io/nvcodec_image_loader.hpp
+++ src/io/nvcodec_image_loader.hpp
@@ -38,7 +38,8 @@ namespace lfs::io {
             int max_num_cpu_threads = 0;
             bool enable_fallback = true;
             size_t decoder_pool_size = 8;
-            bool enable_device_allocator = true;
+            // Opt-in: RTX 4090 bicycle gate measured +35 MiB net and 3.5x dl_wait with the hook enabled.
+            bool enable_device_allocator = false;
             size_t device_allocator_budget_bytes = size_t(256) << 20;
         };
 
